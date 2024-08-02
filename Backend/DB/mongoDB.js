f2ddded96627e0bc0b9e2ed552c4ff253c73b94c
Veena Vidyanandan <106391333+VeenaV12@users.noreply.github.com>
2024-08-02 14:38:04 +0530

    initial commit

---
+++ Backend/DB/mongoDB.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const mongoURI = process.env.MONGODB_URL;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 10000, // Adjust the time in milliseconds (e.g., 10 seconds)
+  socketTimeoutMS: 45000,          // Adjust the time in milliseconds (e.g., 45 seconds)
+};
+
+mongoose.connect(mongoURI, options)
+  .then(() => {
+    console.log('Connected to DB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to DB:', error);
+  });
---
+++ Backend/app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+require('./DB/mongoDB')
+
+const app = express()
+app.use(express.json());
+app.use(cors());
+
+app.use(cors({
+  origin:"https://group2vercel-frontend.vercel.app",
+  credentials:true,
+  methods:['POST','GET','DELETE','PUT']
+}))
+
+app.use('/uploads', express.static('Uploads'));
+
+const authRouter = require('./Routers/authRouter');
+const projectRouter = require('./Routers/projectRouter')
+const referenceRouter = require('./Routers/referenceRouter')
+const forumRouter = require('./Routers/forumRouter')
+const submitRouter = require('./Routers/Postroutes');
+
+app.use('/api', authRouter);
+app.use('/api', projectRouter);
+app.use('/api', referenceRouter)
+app.use('/api', forumRouter)
+
+
+// app.get('/*',function(req,res){res.sendFile(path.join(__dirname,'../Frontend/index.html'));});
+
+app.use('/api', submitRouter);
+
+const port = process.env.PORT || 3001
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
