f0b24422bb4fc108de9bbebc4d30648438559b41
Veena Vidyanandan <106391333+VeenaV12@users.noreply.github.com>
2024-08-02 15:03:04 +0530

    changes in mongodb

--- Backend/DB/mongoDB.js
+++ Backend/DB/mongoDB.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const mongoURI = process.env.MONGODB_URL;
 
 const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
   serverSelectionTimeoutMS: 10000, // Adjust the time in milliseconds (e.g., 10 seconds)
   socketTimeoutMS: 45000,          // Adjust the time in milliseconds (e.g., 45 seconds)
 };
